07aaf8990f0c53de2765b6b61647ce14b53e2ddb
Gerardo Vargas <gerardo.vargas@apscreativas.com>
2021-07-19 16:52:41 -0500

    se guarda el valor del fetch en setRcetea

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -8,6 +8,22 @@ const ModalProvider = (props) => {
 
     //state del provider
     const [idReceta, setIdReceta] = useState(null)
+    const [receta, setReceta] = useState({})
+
+    //cuando tengamos el ID de una receta entonces ejecutamos la API
+
+    useEffect(() => {
+        const getReceta = async () => {
+            if(!idReceta) return
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`
+            const resultado = await axios.get(url)
+            console.log(resultado.data.drinks)
+            setReceta(resultado.data.drinks)
+
+        }
+        getReceta()
+    }, [idReceta])
 
     return (
         <ModalContext.Provider
