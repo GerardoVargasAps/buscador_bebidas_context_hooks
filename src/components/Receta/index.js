c2ab675e7da1c64ab0bc329aa47d42ffd8ca01cf
Gerardo Vargas <gerardo.vargas@apscreativas.com>
2021-07-19 17:54:02 -0500

    se agrega material UI, se crea el modal de Material UI

--- src/components/Receta/index.js
+++ src/components/Receta/index.js
@@ -1,8 +1,51 @@
-import React, { useContext } from 'react';
-
+import React, { useContext, useState } from 'react';
 import { ModalContext } from '../../context/ModalContext';
 
+import { Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+function getModalStyle(){
+
+    // definimos la ubicación del modal
+    const top = 50
+    const left = 50
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`
+    }
+}
+
+// le agregamos unos estilos al modal
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 600,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2,4,3)
+    }
+}))
+
+
 const Receta = ({ receta }) => {
+
+    //Configuración del modal de material UI
+
+    const [ modalStyle ] = useState(getModalStyle)
+    const [open, setOpen] = useState(false)
+    const classes = useStyles()
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    console.log('modal >>>', open)
 
     //extraemos los valores del Modal Context
     const { setIdReceta } = useContext(ModalContext)
@@ -16,10 +59,29 @@ const Receta = ({ receta }) => {
                 <button
                     type="button" 
                     className="btn btn-block btn-primary"
-                    onClick={() => setIdReceta(receta.idDrink)}
+                    onClick={() => {
+                        setIdReceta(receta.idDrink)
+                        handleOpen()
+                    }}
                 >
                     Ver Receta
                 </button>
+                
+                {/* Agregamos el modal y le pasamos los estilos anteriormente declarados */}
+                <Modal
+                    open={open}
+                    onClose={() => {
+                        setIdReceta(null)
+                        handleClose()
+                    }}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                >
+                    <div style={modalStyle} className={classes.paper}>
+                        <h1 className="text-secondary">Desde modal</h1>
+
+                    </div>
+                </Modal>
             </div>
         </div>
        </div>
