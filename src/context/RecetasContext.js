d0e9bf427d93562f41d07ebcbe5c7429db54d6b8
Gerardo Vargas <gerardo.vargas@apscreativas.com>
2021-07-19 15:33:43 -0500

    se agrega el provider de recetas

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -4,9 +4,23 @@ import axios from 'axios';
 export const RecetasContext = createContext()
 
 const RecetasProvider = (props) => {
-    return ( 
 
-     );
+    const [recetas, setRecetas] = useState([])
+    const [busqueda, saveBusqueda] = useState({
+        nombre: '',
+        categoria: ''
+    })
+
+    //retornamos los datos
+    return (
+        <RecetasContext.Provider
+            value={{
+                saveBusqueda
+            }}
+        >
+            {props.children}
+        </RecetasContext.Provider>
+    )
 }
  
 export default RecetasProvider;
